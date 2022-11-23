8401f536d47cf93917165762dedcdc6b2d67b2c1
chrikulhan <98656265+chrikulhan@users.noreply.github.com>
2022-11-22 18:09:33 -0600

    vid3 timestamp 10:36. pause for dinner. This is very straight forward and logical. No wonder the bootcamps are a year. *why so many dollars, though?*

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,4 @@
+<!--This is the main place, all other things are will be referenced from here.-->
 <template>
   <div id="app">
 <!--    template child must have *exactly* one element
@@ -5,7 +6,16 @@
 this one div.-->
     <h1>Would you rather... </h1>
 <!--    you MUST have closed tags when using vue-->
-<!--How to access components in -->
+<!--how to include other templates by their name:-->
+<!--    another component: suggested by webstorm (looks for uppercase followed by lowercase combos):
+This will be replaced by the "WouldYouRather.vue" <div></div> template
+** This is useful as it's has all it's own data, rules, logic, etc. can just add it.-->
+    <would-you-rather></would-you-rather>
+<!--    ^^^this is convention because there is no confusion
+with any html components, as there are no dashing in html names.-->
+<!--    could be written as:
+<WouldYouRather></>WouldYouRather>, page will be identical-->
+
   </div>
   </template>
 
@@ -16,6 +26,16 @@ export default {
   name: 'App',
   components: {
     WouldYouRather
+  },
+//  after the set-up, HERE is where you add data:
+  data() {
+    return {
+    wyrQuestion: 'Would you rather never have to take a bath/shower but still always smell nice or never have to get another shot but still be healthy?',
+      wyrAnswer1: 'Never have to take a bath/shower, but always smell nice.',
+      wyrAnswer2: 'Never have to get another shot but still be healthy.',
+      userSelectionMessage: ''
+    //  ^^empty string because nothing has been chosen yet.
+    }
   }
 }
 </script>
--- src/components/WouldYouRather.vue
+++ src/components/WouldYouRather.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="hello">
-<!--    when you add something here, -->
+<!--    when you add something here, it's
+ not added to the page because everything seen
+ on the webpage is app.vue, and it's just one h1 element
+ **What we do to incorporate components in our page is reference
+ them here (in WouldYouRather.vue) in this template-->
   <h2>Please make your choice.</h2>
 
   </div>
